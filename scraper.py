adc0a42f544a1f23e10ec276981cf9fa5658b687
Snowyblack58 <davidzhao058@gmail.com>
2016-10-03 12:10:23 -0400

    Begin basic statistical analysis on the hour distribution

--- scraper.py
+++ scraper.py
@@ -8,6 +8,10 @@ def login(driver):
 	driver.find_element_by_id('pass').send_keys(creds['password'])
 	driver.find_element_by_id('pass').submit()
 
+'''
+Get distribution of hours spent outside of the classroom for the specified course
+The distribution is [<1, 1-3, 4-6, 7-9, 10+] hours
+'''
 def getHoursSpentDistribution(group, number):
 	driver = webdriver.Firefox()
 	driver.get('https://evals.itc.virginia.edu/course-selectionguide/pages/SGMain.jsp?cmp=' + group + ',' + number + ',')
@@ -20,9 +24,18 @@ def getHoursSpentDistribution(group, number):
 		votes = int(re.search('\d+', responses[cnt].text).group(0))
 		for i in range(5):
 			distribution[i] += round(float(percentages[cnt*5+i].text[:-1]) / 100 * votes)
+	driver.close()
 	return distribution
+
+def printStatistics(distribution):
+	medianIndex = round(sum(distribution) / 2)
+	for i in range(5):
+		if sum(distribution[:i]) > medianIndex:
+			break
+	print(['<1','1-3','4-6','7-9','10+'][i-1])
 
 def main():
 	distribution = getHoursSpentDistribution('APMA','1110')
 	print(distribution)
+	printStatistics(distribution)
 main()
