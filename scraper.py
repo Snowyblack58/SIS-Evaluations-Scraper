1272526b3422c9f7c22c23cac07bf7584030b87e
Snowyblack58 <davidzhao058@gmail.com>
2016-10-03 11:56:53 -0400

    Implement the retrieval of the hours spent distribution for a single course

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import yaml
+import re
 
 def login(driver):
 	creds = yaml.load(open('NPcredentialsNP.yml'))
@@ -12,8 +13,16 @@ def getHoursSpentDistribution(group, number):
 	driver.get('https://evals.itc.virginia.edu/course-selectionguide/pages/SGMain.jsp?cmp=' + group + ',' + number + ',')
 	if driver.title == 'NetBadge Web Login':
 		login(driver)
-
+	percentages = driver.find_elements_by_class_name('percentage')
+	responses = driver.find_elements_by_xpath('html/body/div/form/table/tbody/tr/td/table/tbody/tr/td/span/span')
+	distribution = [0,0,0,0,0]
+	for cnt in range(0, len(responses)):
+		votes = int(re.search('\d+', responses[cnt].text).group(0))
+		for i in range(5):
+			distribution[i] += round(float(percentages[cnt*5+i].text[:-1]) / 100 * votes)
+	return distribution
 
 def main():
-	getHoursSpentDistribution('APMA','1110')
+	distribution = getHoursSpentDistribution('APMA','1110')
+	print(distribution)
 main()
